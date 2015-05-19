c1a1e3269af0985e02b4bddefe90a421dd517d7a
Kamlesh Verma <kaverma@localhost.localdomain>
2015-05-19 16:13:15 +0530

    discovered hosts notification implementation
    show all discovered hosts in notification list (modifiedi and added scroll bar)
    
    Signed-off-by: Kamlesh Verma <kaverma@localhost.localdomain>

--- app/scripts/components/requests/controllers/requests.js
+++ app/scripts/components/requests/controllers/requests.js
@@ -2,17 +2,17 @@
 (function() {
     'use strict';
     define(['lodash'], function(_) {
-        var RequestsController = function($scope, $interval, UserService, RequestTrackingService, ServerService) {
+        var RequestsController = function($scope, $interval, UserService, RequestTrackingService, ServerService, UtilService, RequestService, $log, $timeout) {
             $scope.tasks = [];
-            $scope.discoveredHosts = [];
+            $scope.discoveredHostsLength = 0;
             $scope.reloadTasks = function() {
                 RequestTrackingService.getTrackedRequests().then(function(tasks) {
                     $scope.tasks = tasks;
                 });
             }
-            $scope.reloadDiscoveredHosts = function() {
+            $scope.reloadDiscoveredHostsLength = function() {
                 ServerService.getDiscoveredHosts().then(function(freeHosts) {
-                   $scope.discoveredHosts = freeHosts;
+                  $scope.discoveredHostsLength = freeHosts.length;
                 });
             }
 
@@ -20,9 +20,72 @@
                UserService.logout();
             }
 
+            $scope.getDiscoveredHosts = function() {
+                $scope.discoveredHosts = [];
+                ServerService.getDiscoveredHosts().then(function(freeHosts) {
+                   _.each(freeHosts, function(freeHost) {
+                    var host = {
+                        hostname: freeHost.node_name,
+                        ipaddress: freeHost.management_ip,
+                        state: "UNACCEPTED",
+                        selected: false
+                    };
+                    $scope.discoveredHosts.push(host);
+                   });
+                });
+            }
+
+            $scope.acceptHost = function(host) {
+                var hosts = {
+                    nodes: [
+                        {
+                            node_name: host.hostname,
+                            management_ip: host.ipaddress
+                        }
+                    ]
+                };
+                UtilService.acceptHosts(hosts).then(function(result) {
+                    $log.info(result);
+                    host.state = "ACCEPTING";
+                    host.task = result;
+                    var callback = function() {
+                        RequestService.get(result).then(function (request) {
+                            if (request.status === 'FAILED' || request.status === 'FAILURE') {
+                                $log.info('Failed to accept host in requests controller' + host.hostname);
+                                host.state = "FAILED";
+                                host.task = undefined;
+                            }
+                            else if (request.status === 'SUCCESS'){
+                                $log.info('Accepted host in requests controller ' + host.hostname);
+                                host.state = "ACCEPTED";
+                                host.task = undefined;
+                            }
+                            else {
+                                $log.info('Accepting host in requests controller' + host.hostname);
+                                $timeout(callback, 5000);
+                            }
+                        });
+                    }
+                    $timeout(callback, 5000);
+                });
+            };
+
+            $scope.openDiscoveredHostsModel = function() {
+                document.getElementById("openDiscoveredHosts").click();
+            }
+
+            $scope.acceptAllHosts = function() {
+                _.each($scope.discoveredHosts, function(host) {
+                    if(host.state === "UNACCEPTED") {
+                        $scope.acceptHost(host);
+                    }
+                });
+            };
+
             $interval($scope.reloadTasks, 5000);
-            $interval($scope.reloadDiscoveredHosts, 5000);
+            $interval($scope.reloadDiscoveredHostsLength, 5000);
         };
-        return ['$scope', '$interval', 'UserService', 'RequestTrackingService', 'ServerService', RequestsController];
+
+        return ['$scope', '$interval', 'UserService', 'RequestTrackingService', 'ServerService', 'UtilService', 'RequestService', '$log', '$timeout', RequestsController];
     });
 })();
--- app/scripts/controllers/first.js
+++ app/scripts/controllers/first.js
@@ -3,10 +3,11 @@
     'use strict';
     define(['lodash', 'helpers/modal-helpers'], function(_) {
 
-        var FirstTimeController = function($location, ClusterService) {
+        var FirstTimeController = function($location, ClusterService, ServerService, UtilService, $log, $timeout, RequestService) {
+            var self = this;
+            this.discoveredHosts = [];
             ClusterService.getList().then(function(clusters) {
                 if(clusters.length > 0) {
-                    $location.path('/');
                 }
             });
 
@@ -17,7 +18,62 @@
             this.importCluster = function() {
                 $location.path('/clusters/import');
             };
+
+            ServerService.getDiscoveredHosts().then(function(freeHosts) {
+               _.each(freeHosts, function(freeHost) {
+                var host = {
+                    hostname: freeHost.node_name,
+                    ipaddress: freeHost.management_ip,
+                    state: "UNACCEPTED",
+                    selected: false
+                };
+                self.discoveredHosts.push(host);
+               });
+            });
+           
+            this.acceptHost = function(host) {
+                var hosts = {
+                    nodes: [
+                        {
+                            node_name: host.hostname,
+                            management_ip: host.ipaddress
+                        }
+                    ]
+                };
+                UtilService.acceptHosts(hosts).then(function(result) {
+                    $log.info(result);
+                    host.state = "ACCEPTING";
+                    host.task = result;
+                    var callback = function() {
+                        RequestService.get(result).then(function (request) {
+                            if (request.status === 'FAILED' || request.status === 'FAILURE') {
+                                $log.info('Failed to accept host in first controller' + host.hostname);
+                                host.state = "FAILED";
+                                host.task = undefined;
+                            }
+                            else if (request.status === 'SUCCESS'){
+                                $log.info('Accepted host in first controller ' + host.hostname);
+                                host.state = "ACCEPTED";
+                                host.task = undefined;
+                            }
+                            else {
+                                $log.info('Accepting host in first controller' + host.hostname);
+                                $timeout(callback, 5000);
+                            }
+                        });
+                    }
+                    $timeout(callback, 5000);
+                });
+            };
+
+            this.acceptAllHosts = function() {
+                _.each(self.discoveredHosts, function(host) {
+                    if(host.state === "UNACCEPTED") {
+                        self.acceptHost(host);
+                    }
+                });
+            };
         };
-        return ['$location', 'ClusterService', FirstTimeController];
+        return ['$location', 'ClusterService', 'ServerService', 'UtilService', '$log', '$timeout', 'RequestService', FirstTimeController];
     });
 })();
