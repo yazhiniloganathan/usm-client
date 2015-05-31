4084ff2cd01fba6b12ec97e3e0236075f6d19fb4
Kanagaraj M <kmayilsa@redhat.com>
2015-05-31 15:28:48 +0530

    fix pool/utilization heatmap in dashboard
    Replace mock data with live data.
    
    Signed-off-by: Kanagaraj M <kmayilsa@redhat.com>

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -50,7 +50,6 @@
             self.trendCapacity.values = [];
             self.trendCapacity.selected = { used: 0, isTotal: true, type: '' };
 
-
             this.calculateTotalCapacity = function() {
                 var byType = [0, 0, 0]; // block, file, object
                 var byTier = [0, 0, 0]; // default, fast, slower
@@ -135,6 +134,14 @@
                         bandwidth: bandwidth,
                         bandwidthFormatted: bandwidthFormatted
                     };
+
+                    VolumeService.getListByCluster(cluster.cluster_id).then(function(volumes) {
+                        cluster.volumes = volumes;
+                    });
+
+                    PoolService.getListByCluster(cluster.cluster_id).then(function(pools) {
+                        cluster.pools = pools;
+                    });
                 });
                 self.calculateTotalCapacity();
             });
@@ -203,38 +210,6 @@
                 });
                 return list;
             };
-
-            this.iopsList = [
-                { name: 'MyCeph1', value: 354000 },
-                { name: 'MyCeph2', value: 297000 },
-                { name: 'MyCeph3', value: 123000 },
-                { name: 'MyCluster1', value: 96000 },
-                { name: 'MyCluster2', value: 92000 }
-            ];
-
-            this.bandwidthList = [
-                { name: 'MyCeph1', value: 35000 },
-                { name: 'MyCeph2', value: 27000 },
-                { name: 'MyCeph3', value: 13000 },
-                { name: 'MyCluster1', value: 9000 },
-                { name: 'MyCluster2', value: 2000 }
-            ];
-
-            this.cpuList = [
-                { name: 'Host1', value: 96 },
-                { name: 'Host6', value: 89 },
-                { name: 'Host2', value: 87 },
-                { name: 'Host9', value: 84 },
-                { name: 'Host3', value: 81 }
-            ];
-
-            this.memoryList = [
-                { name: 'Host1', value: 98 },
-                { name: 'Host6', value: 93 },
-                { name: 'Host2', value: 88 },
-                { name: 'Host9', value: 85 },
-                { name: 'Host3', value: 78 }
-            ];
 
             this.clusterList = [];
             _.each(_.range(6), function(index) {
