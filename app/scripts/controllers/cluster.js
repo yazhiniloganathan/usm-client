63c035d4bb4c56de28c8110dd0f470e43710519c
Kamlesh Verma <kaverma@localhost.localdomain>
2015-06-01 12:36:48 +0530

    cluster blade view modification
    implemented cluster blade view (modified again)
    
    Signed-off-by: Kamlesh Verma <kaverma@localhost.localdomain>

--- app/scripts/controllers/cluster.js
+++ app/scripts/controllers/cluster.js
@@ -3,7 +3,7 @@
     'use strict';
     define(['lodash', 'helpers/cluster-helpers', 'helpers/mock-data-provider-helpers', 'c3'], function(_, ClusterHelpers, MockDataProviderHelpers, c3) {
         window.c3 = c3;
-        var ClusterController = function($scope, $location, $interval, ClusterService, ServerService, VolumeService, PoolService) {
+        var ClusterController = function($scope, $location, $interval, ClusterService, ServerService, VolumeService, PoolService, $q) {
             ClusterService.getList().then(function(clusters) {
                 if(clusters.length === 0) {
                     $location.path('/first');
@@ -12,12 +12,21 @@
 
             $scope.clusters = [];
 
-            getAllClusters();
-
-            function getAllClusters() {
-                $scope.clusters = [];
+            var timer = $interval(reloadData, 15000);
+            $scope.$on('$destroy', function() {
+                $interval.cancel(timer);
+            });
+            reloadData();
+            
+            var reloading = false;
+            function reloadData() {
+                if(reloading) {
+                    return;
+                }
+                reloading = true;
                 ClusterService.getList().then(function(clusters){
-                    _.each(clusters, function(cluster) {
+                     var tempClusters = [];
+                     _.each(clusters, function(cluster) {
                         var mockCluster = MockDataProviderHelpers.getMockCluster(cluster.cluster_name);
                         var tempCluster = {
                             cluster_id : cluster.cluster_id,
@@ -25,36 +34,59 @@
                             cluster_type : cluster.cluster_type,
                             storage_type : cluster.storage_type,
                             cluster_status : cluster.cluster_status,
-                            no_of_hosts : 0,
-                            no_of_volumes_or_pools : 0,
-                            description : mockCluster.description,
-                            data_used : mockCluster.data_used,
-                            alerts : mockCluster.alerts,
+                            used : cluster.used,
                             areaSpline_cols : [{ id:1, name: 'Used', color: '#39a5dc', type: 'area-spline' }],
                             areaSpline_values : mockCluster.areaSpline_values,
-                            gauge_cols : [ { id:1, name: 'Used', color: $scope.getClusterGaugeColor(mockCluster.gauge_values[0]['1']), type: 'gauge' }],
-                            gauge_values : mockCluster.gauge_values
+                            gauge_values : mockCluster.gauge_values[0]['1'],
+                            alerts : mockCluster.alerts
                         };
-                        ServerService.getListByCluster(cluster.cluster_id).then(function (hosts) {
-                             tempCluster.no_of_hosts = hosts.length;
-                             if($scope.getClusterTypeTitle(cluster.cluster_type) === 'Gluster') {
-                                VolumeService.getListByCluster(cluster.cluster_id).then(function (volumes) {
-                                     tempCluster.no_of_volumes_or_pools = volumes.length;
-                                     $scope.clusters.push(tempCluster);
-                                });
-                             }else {
-                                PoolService.getListByCluster(cluster.cluster_id).then(function (pools) {
-                                     tempCluster.no_of_volumes_or_pools = pools.length;
-                                     $scope.clusters.push(tempCluster);
-                                });
-                             }
+                        if($scope.getClusterTypeTitle(cluster.cluster_type) === 'Gluster') {
+                            VolumeService.getListByCluster(cluster.cluster_id).then(function (volumes) {
+                                 tempCluster.no_of_volumes_or_pools = volumes.length;
+                            });
+                        }else {
+                            PoolService.getListByCluster(cluster.cluster_id).then(function (pools) {
+                                 tempCluster.no_of_volumes_or_pools = pools.length;
+                            });
+                        }
+                        tempClusters.push(tempCluster);
+                    });
+                    
+                    var hosts = [];
+                    var sizes = [];
+                    _.each(tempClusters, function(cluster) {
+                        hosts.push(ServerService.getListByCluster(cluster.cluster_id));
+                        sizes.push(ClusterService.getCapacity(cluster.cluster_id));
+                    });
+                    $q.all(hosts).then(function(hostList) {
+                        var index = 0;
+                        _.each(hostList, function(host) {
+                            tempClusters[index].no_of_hosts = host.length;
+                            index++;
                         });
                     });
+                    $q.all(sizes).then(function(sizeList) {
+                        var index = 0;
+                        _.each(sizeList, function(size) {
+                            tempClusters[index].total_size = size;
+                            tempClusters[index].free_size = size - tempClusters[index].used;
+                            tempClusters[index].percent_used =  isNaN(Math.round(tempClusters[index].used*(100/size))) ? 0 : Math.round(tempClusters[index].used*(100/size));
+                            index++;
+                        });
+                        $scope.clusters = tempClusters;
+                        reloading = false;
+                    });
+                    if($scope.clusters.length === 0) {
+                        $scope.clusters = tempClusters;
+                    }
                 });
-            }
+            };
 
             $scope.getClusterGaugeColor = function(gauge_value) {
-                return (0<=gauge_value && gauge_value<=75) ? 'green' : ((76<=gauge_value && gauge_value<=85) ? 'orange' : 'red')
+                gauge_value = gauge_value * 10;
+                if (gauge_value >= 90) return '#E35C5C';
+                if (gauge_value >= 80) return '#FF8C1B';
+                else return '#4AD170';
             };
 
             $scope.getClusterTypeTitle = function(type) {
@@ -79,17 +111,11 @@
 
             $scope.remove = function(clusterId) {
                 ClusterService.remove(clusterId).then(function(result){
-                    getAllClusters();
+                    reloadData();
                 });
             };
 
-            $scope.isDeleteAvailable = function() {
-                return _.filter($scope.clusters, function(cluster){
-                    return cluster.selected;
-                }).length;
-            }
-           
         };
-        return ['$scope', '$location', '$interval', 'ClusterService', 'ServerService', 'VolumeService', 'PoolService', ClusterController];
+        return ['$scope', '$location', '$interval', 'ClusterService', 'ServerService', 'VolumeService', 'PoolService', '$q', ClusterController];
     });
 })();
